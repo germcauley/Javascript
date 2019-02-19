a9e57dcf33d5970b5ceca42691b9f8e2f9080054
Gerald McAuley <geraldmcauley@gmail.com>
2019-02-19 14:35:41 +0000

    Add files via upload

--- CalculatingWithFunction.js
+++ CalculatingWithFunction.js
@@ -1,39 +1,156 @@
-// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
 
-// JavaScript:
+//Operators
+function plus(number) {
+    return " + " + number.toString()
+  }
+function minus(number) {
+    return " - " + number.toString()
+  }
 
-// seven(times(five())); // must return 35
-// four(plus(nine())); // must return 13
-// eight(minus(three())); // must return 5
-// six(dividedBy(two())); // must return 3
+function times(number) {
+    return " * " + number.toString()
+  }
 
-// Ruby:
+function dividedBy(number) {
+    return " / " + number.toString()
+  }
+  
+//Numbers
+function zero(callback) {
+    value = 0;
+    
+  if(arguments.length == 0) // Means no parameters are passed  
+  {  
+      return value
+  } 
+  else if(arguments.length == 1) // Means one parameter is are passed  
+  {  
+    return Math.floor(eval(value+arguments[0])); 
+  }
+}
 
-// seven(times(five)) # must return 35
-// four(plus(nine)) # must return 13
-// eight(minus(three)) # must return 5
-// six(divided_by(two)) # must return 3
+function one(callback) {
+      value = "1";
+      
+    if(arguments.length == 0) // Means no parameters are passed  
+    {  
+        return value
+    } 
+    else if(arguments.length == 1) // Means one parameter is are passed  
+    {  
+      return Math.floor(eval(value+arguments[0])); 
+    }
+  }
 
-// Requirements:
+function two(callback) {
+    value = 2;
+    
+  if(arguments.length == 0) // Means no parameters are passed  
+  {  
+      return value
+  } 
+  else if(arguments.length == 1) // Means one parameter is are passed  
+  {  
+    return Math.floor(eval(value+arguments[0]));  
+  }
+}
 
-//     There must be a function for each number from 0 ("zero") to 9 ("nine")
-//     There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy (divided_by in Ruby)
-//     Each calculation consist of exactly one operation and two numbers
-//     The most outer function represents the left operand, the most inner function represents the right operand
-//     Divison should be integer division, e.g eight(dividedBy(three()))/eight(divided_by(three)) should return 2, not 2.666666...
+function three(callback) {
+  value = 3;
+  
+if(arguments.length == 0) // Means no parameters are passed  
+{  
+    return value
+} 
+else if(arguments.length == 1) // Means one parameter is are passed  
+{  
+   return Math.floor(eval(value+arguments[0])); 
+}
+}
 
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function four(callback) {
+  value = 4;
+  
+if(arguments.length == 0) // Means no parameters are passed  
+{  
+    return value
+} 
+else if(arguments.length == 1) // Means one parameter is are passed  
+{  
+  return Math.floor(eval(value+arguments[0])); 
+}
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function five(callback) {
+  value = 5;
+  
+if(arguments.length == 0) // Means no parameters are passed  
+{  
+    return value
+} 
+else if(arguments.length == 1) // Means one parameter is are passed  
+{  
+  return Math.floor(eval(value+arguments[0])); 
+}
+}
+
+function six(callback) {
+  value = 6;
+  
+if(arguments.length == 0) // Means no parameters are passed  
+{  
+    return value
+} 
+else if(arguments.length == 1) // Means one parameter is are passed  
+{  
+  return Math.floor(eval(value+arguments[0]));  
+}
+}
+
+function seven(callback) {
+  value = 7;
+  
+if(arguments.length == 0) // Means no parameters are passed  
+{  
+    return value
+} 
+else if(arguments.length == 1) // Means one parameter is are passed  
+{  
+  return Math.floor(eval(value+arguments[0]));  
+}
+}
+
+function eight(callback) {
+  value = 8;
+  
+if(arguments.length == 0) // Means no parameters are passed  
+{  
+    return value
+} 
+else if(arguments.length == 1) // Means one parameter is are passed  
+{  
+   return Math.floor(eval(value+arguments[0])); 
+}
+}
+
+function nine(callback) {
+  value = 9;
+  
+if(arguments.length == 0) // Means no parameters are passed  
+{  
+    return value
+} 
+else if(arguments.length == 1) // Means one parameter is are passed  
+{  
+   return Math.floor(eval(value+arguments[0])); 
+}
+}
+  
+
+  
+
+  console.log(eight(dividedBy(three())));
+
+
+
+

c505d6442cf838b9493b817bc80accd12b61f8e5
Gerald McAuley <geraldmcauley@gmail.com>
2019-03-05 17:14:23 +0000

    Object creation established
    Script now takes input and breaks it into and object. Can also retrieve the keys etc from dictionary array. Next step is to compare input obj to index obj and finally output index word with most key/value matches to input word

--- Did_You_Mean.js
+++ Did_You_Mean.js
@@ -16,32 +16,50 @@ function Dictionary(words) {
   
   Dictionary.prototype.findMostSimilar = function(term) {
     // TODO: this is your task ;)
+    const word = createObject(term);
+    const wordKeys= Object.keys(word)
+
+    //for loop for length of dictionary
+    for(var i =0; i < this.words.length; i++){
+        console.log(this.words[i]);
+        console.log(createObject(this.words[i]));
+        //create object from array index
+        let index = createObject(this.words[i]);
+        //get keys of the object
+        let indArr = Object.keys(index);
+        //keys in input word
+        console.log(wordKeys);
+        //keys in dictionary index
+        console.log(indArr);
+       
+    }
+    return 'hello';
+    
+    
   }
-
-
 
 //   Create object to index occurences of letters in input
 // Run same code on each word from dictionary array and use the one with most matches
 
-var test = "goodbye";
-const 
-
-
-newArr = test.split('');
-var strObj = {};
 
 //Break word into and obejct with occurnce of letters counted
-for(var i =0; i < newArr.length; i++){
-    if(!strObj[newArr[i]]){
-        strObj[newArr[i]]=1;
-    }
-    else{
-        strObj[newArr[i]]+=1;
-    }
-    console.log(newArr[i]);
-    console.log(strObj);
-
+function createObject(input){
+  newArr = input.split('');
+  var strObj = {};
+  for(var i =0; i < newArr.length; i++){
+      if(!strObj[newArr[i]]){
+          strObj[newArr[i]]=1;
+      }
+      else{
+          strObj[newArr[i]]+=1;
+      }
+  }
+  return strObj;
 }
+
+
+things = new Dictionary(['stars', 'mars', 'wars', 'codec', 'codewars']);
+console.log(things.findMostSimilar('coddwars')); 
 
 
 

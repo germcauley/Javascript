aaf20cd6c90c00f43eddb638ac77699abc246203
Gerald McAuley <geraldmcauley@gmail.com>
2019-03-07 22:20:42 +0000

    removed  commnt

--- Did_You_Mean.js
+++ Did_You_Mean.js
@@ -7,7 +7,6 @@
 
 // languages = new Dictionary(['javascript', 'java', 'ruby', 'php', 'python', 'coffeescript']);
 // languages.findMostSimilar('heaven'); // must return "java"
-// languages.findMostSimilar('javascript'); // must return "javascript" (same words are obviously the most similar ones)
 
 
 function Dictionary(words) {
@@ -18,19 +17,26 @@ function Dictionary(words) {
     // TODO: this is your task ;)
     const word = createObject(term);
     const wordKeys= Object.keys(word)
+    var matchArr=[];
 
     //for loop for length of dictionary
     for(var i =0; i < this.words.length; i++){
-        console.log(this.words[i]);
-        console.log(createObject(this.words[i]));
+        console.log(this.words[i]);//array word
+        console.log(createObject(this.words[i]));//array key/values
         //create object from array index
         let index = createObject(this.words[i]);
         //get keys of the object
         let indArr = Object.keys(index);
         //keys in input word
-        console.log(wordKeys);
+        //console.log(wordKeys);
+        console.log(term);//input term
+        console.log(word);//input key/values
         //keys in dictionary index
-        console.log(indArr);
+        //console.log(indArr);
+        console.log("-------------------");
+
+        //compare keys of each object
+        
        
     }
     return 'hello';
@@ -38,11 +44,11 @@ function Dictionary(words) {
     
   }
 
-//   Create object to index occurences of letters in input
+// Create object to index occurences of letters in input
 // Run same code on each word from dictionary array and use the one with most matches
 
 
-//Break word into and obejct with occurnce of letters counted
+//Break word into an object with occurences of letters counted
 function createObject(input){
   newArr = input.split('');
   var strObj = {};

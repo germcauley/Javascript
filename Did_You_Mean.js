e416cafe8d4b666e71385c3b129557defbb75565
Gerald McAuley <geraldmcauley@gmail.com>
2019-03-12 16:29:45 +0000

    Almost there,
    basic tests pass bu t advanced tests dont!

--- Did_You_Mean.js
+++ Did_You_Mean.js
@@ -1,110 +1,71 @@
-// fruits = new Dictionary(['cherry', 'pineapple', 'melon', 'strawberry', 'raspberry']);
-// fruits.findMostSimilar('strawbery'); // must return "strawberry"
-// fruits.findMostSimilar('berry'); // must return "cherry"
-
-// things = new Dictionary(['stars', 'mars', 'wars', 'codec', 'codewars']);
-// things.findMostSimilar('coddwars'); // must return "codewars"
-
-// languages = new Dictionary(['javascript', 'java', 'ruby', 'php', 'python', 'coffeescript']);
-// languages.findMostSimilar('heaven'); // must return "java"
-
-
 function Dictionary(words) {
-    this.words = words;
-  }
-  
-  Dictionary.prototype.findMostSimilar = function(term) {
-    // TODO: this is your task ;)
-    const word = createObject(term);
-    const wordKeys= Object.keys(word)
-    var matches =0; //number of matches
-    var matchWord ='';
-    var matchWordCount =0;
-
-    //for loop for length of dictionary
-    for(var i =0; i < this.words.length; i++){
-      
-        var arrayWord = this.words[i];
-        //console.log(arrayWord);//array word
-        //console.log(createObject(arrayWord));//array key/values
-
-        //create object from array index, this is current word from array
-        let index = createObject(arrayWord);
-        //get keys of the object
-        //let indArr = Object.keys(index);
-
-        // console.log('------------------------------NEW WORDS----------------------');
-        // console.log(term);//input term
-        // console.log(word);//input key/values
-        // console.log(arrayWord);
-        // console.log(index);
-        // //keys in dictionary index
-        // //console.log(indArr);
-        // console.log("-------------------");
-
-        //run for each key in word
-       for(var p in word){ 
-         //run over each key in index, compare with word keys
-        //  console.clear()
-        //  console.log('Comparing '+ p + ' '+word[p]+' with...... ');
-         for(var q in index){
-            // console.log(q+' '+index[q]);
-           if(p === q){
-            // console.log('MATCH!');
-            matches +=1;
-            break;
-           }
-         }
-         
-       } 
-       if(matches >matchWordCount ){
-          matchWord = arrayWord;
-          matchWordCount = matches;
-          // console.log('NEW MATCH WORD '+ matchWord);
-          // console.log('MATCHING LETTER COUNT '+matchWordCount);
+  this.words = words;
+}
+Dictionary.prototype.findMostSimilar = function(term) {
+  // TODO: this is your task ;)
+  const word = createObject(term);
+  var matches =0; //number of matches
+  var finalMatches =0; //number of matches
+  var matchWord ='';
+  var differences =0;
+  var finalDifferences =100;
+  //for loop for length of dictionary
+  for(var i =0; i < this.words.length; i++){
+      var arrayWord = this.words[i];
+      let index = createObject(arrayWord);
+      //run for each key in word
+     for(var p in word){ 
+       //run over each key in index, compare with word keys
+       for(var q in index){  
+         if(p === q){
+          matches += word[p];
+          break;
+         } 
+       }
+     } 
+     differences = getDifference(term.length,arrayWord.length,matches);
+     console.log("INPUT: "+term+" ARRAYWORD: "+arrayWord+ " MATCHES: "+matches+" DIFFERENCES: "+differences);
+     if(matches>=finalMatches&&differences<finalDifferences){
+        matchWord = arrayWord;
+        finalDifferences=differences;
+        finalMatches = matches;
      }
-     else if(matches == matchWordCount ){
-         
-      if(arrayWord.length< matchWord.length){
-       matchWord = arrayWord;
-       matchWordCount = matches;
-       // console.log('NEW MATCH WORD '+ matchWord);
-       // console.log('MATCHING LETTER COUNT '+matchWordCount);
-      }
-      else if(matchWordCount == 0){
-       matchWord = arrayWord;
-       matchWordCount = matches;
-       // console.log('NEW MATCH WORD '+ matchWord);
-       // console.log('MATCHING LETTER COUNT '+matchWordCount);
-      }
+  matches =0;//reset for next word in array
   }
-       var matches =0;//reset for next word in array
-
-    }
-    return matchWord;
-    
-    
-  }
-
+  return matchWord;
+}
 //Break word into an object with occurences of letters counted
 function createObject(input){
-  newArr = input.split('');
-  var strObj = {};
-  for(var i =0; i < newArr.length; i++){
-      if(!strObj[newArr[i]]){
-          strObj[newArr[i]]=1;
-      }
-      else{
-          strObj[newArr[i]]+=1;
-      }
-  }
-  return strObj;
+newArr = input.split('');
+var strObj = {};
+for(var i =0; i < newArr.length; i++){
+    if(!strObj[newArr[i]]){
+        strObj[newArr[i]]=1;
+    }
+    else{
+        strObj[newArr[i]]+=1;
+    }
 }
-
-
-languages = new Dictionary(['cherry', 'pineapple', 'melon', 'strawberry', 'raspberry']);
+return strObj;
+}
+function getDifference(w1,w2,matches){
+  let output =0;
+  if(w1>w2){
+    output = (w1-matches) - (w2-matches);
+    return output;
+  }
+  else if (w2>w1){
+    output = (w2-matches) - (w1-matches);
+    return output;
+  }
+  else if(w1==w2){
+    output = w1 -matches
+    return output;
+  }
+}
+languages = new Dictionary(['javascript', 'java', 'ruby', 'php', 'python', 'coffeescript']);
 // things = new Dictionary(['stars']);
-console.log(languages.findMostSimilar('berry')); 
+console.log(languages.findMostSimilar('heaven')); 
 
 
 

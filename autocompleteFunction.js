98ba6f5c93e17fc113462f125f5ec59bb592a1e1
Gerald McAuley <geraldmcauley@gmail.com>
2019-09-12 16:11:10 +0100

    Add files via upload

--- autocompleteFunction.js
+++ autocompleteFunction.js
@@ -1,43 +1,33 @@
-function autocomplete(input, dictionary){
+function autocomplete(input, dictionary) {
     //get length of word
-    len = (input.length)-1;
+    len = (input.length) - 1;
     //Each word in dictionary
-    resultArr =[];
-    for(var j=0; j <dictionary.length; j++){
-        var match = false 
+    resultArr = [];
+    for (var j = 0; j < dictionary.length; j++) {
+        var match = false
         //letter 1, 2 in word
-        for(var i = 0; i <= len; i++){
-            if(dictionary[j][i] !== input[i]){
-                match = false;
-                break
-            }
-            else{
+        //check if is letter
+
+        for (var i = 0; i <= len; i++) {
+            if (/^[A-Za-z]+$/.test(dictionary[j][i]) && dictionary[j][i] == input[i]) {
+                console.log(dictionary[j][i] + " is a letter")
                 match = true;
-            }   
+            } else {
+                console.log(dictionary[j][i] + " is NOT a letter")
+                match = false;
+            }
+            // if (dictionary[j][i] !== input[i]) {
+            //     match = false;
+            //     break
+            // } else {
+            //     match = true;
+            // }
         }
-        if(match == true){
+        if (match == true) {
             resultArr.push(dictionary[j])
         }
     }
-    return(resultArr)
+    return (resultArr)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-autocomplete('ai', ['airplane','airport','apple','ball'])
-
-
-
-
-// take in an input string and a dictionary array and return the values from the dictionary that start with the input string.
-// autocomplete('ai', ['airplane','airport','apple','ball']) = ['airplane','airport']
+console.log(autocomplete('ai', ['a7irplane', 'apple', 'air', 'a6venue', 'airport', 'adamantium', 'awkwardness', 'awesome', 'amazing']))
